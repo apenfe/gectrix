e263d86f7b761ec05f59c3925a95d9e0a63a0741
Adrián Peñalver Fernández <adrian1414@hotmail.es>
2024-11-21 17:54:12 +0100

    Se comienza migracion de organigrama basico

---
+++ database/migrations/2024_11_21_160818_create_brigades_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brigades', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->enum('army', ['tierra', 'aire', 'armada', 'general', 'infanteria_marina'])->default('tierra');
+            $table->enum('status', ['operativa', 'inoperativa'])->default('operativa');
+            $table->integer('max_subordinates')->default(4);
+            $table->integer('current_subordinates')->default(0);
+            $table->decimal('latitude', 10, 7)->nullable();
+            $table->decimal('longitude', 10, 7)->nullable();
+            $table->string('combat_logo'); // por defecto
+            $table->string('unit_emblem');
+            $table->foreignId('commander_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brigades');
+    }
+};
