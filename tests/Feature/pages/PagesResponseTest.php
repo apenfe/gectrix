3c98e8f979cf0b2b20c4f93752306a9e49fc6229
Adrián Peñalver Fernández <adrian1414@hotmail.es>
2024-12-08 18:59:07 +0100

    se crea rama de modulo de alerta temprana

--- tests/Feature/pages/PagesResponseTest.php
+++ tests/Feature/pages/PagesResponseTest.php
@@ -1,6 +1,17 @@
 <?php
 
+use App\Models\Battalion;
+use App\Models\Brigade;
+use App\Models\Commander;
+use App\Models\Company;
+use App\Models\Platoon;
+use App\Models\Regiment;
+use App\Models\Section;
+use App\Models\Soldier;
+use App\Models\Squad;
 use App\Models\User;
+use App\Models\Vehicle;
+use App\Models\Weapon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use function Pest\Laravel\get;
 
@@ -12,5 +23,38 @@ it('gives back successful response for dashboard', function () {
     $this->actingAs($user);
 
     get(route('dashboard'))
+        ->assertOk();
+});
+
+it('gives back successful response for personal', function () {
+
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    Weapon::factory()->count(10)->create();
+
+    Commander::factory(['weapon_id' => 1])->create();
+    Commander::factory(['weapon_id' => 2])->create();
+    Commander::factory(['weapon_id' => 3])->create();
+    Commander::factory(['weapon_id' => 4])->create();
+    Commander::factory(['weapon_id' => 5])->create();
+    Commander::factory(['weapon_id' => 6])->create();
+    Commander::factory(['weapon_id' => 7])->create();
+
+    Brigade::factory(['commander_id' => 1])->create();
+    Regiment::factory(['brigade_id' => 1, 'commander_id' => 2])->create();
+    Battalion::factory(['regiment_id' => 1, 'commander_id' => 3])->create();
+    Company::factory(['battalion_id' => 1, 'commander_id' => 4])->create();
+    Section::factory(['company_id' => 1, 'commander_id' => 5])->create();
+    Platoon::factory(['section_id' => 1, 'commander_id' => 6])->create();
+    Squad::factory(['platoon_id' => 1, 'commander_id' => 7])->create();
+
+    Soldier::factory([ 'squad_id' => 1, 'weapon_id' => 8])->create();
+    Soldier::factory([ 'squad_id' => 1, 'weapon_id' => 9])->create();
+    Soldier::factory([ 'squad_id' => 1, 'weapon_id' => 10])->create();
+
+    Vehicle::factory()->count(1)->create();
+
+    get(route('personal.index'))
         ->assertOk();
 });
